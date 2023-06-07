82f3ea8b22c3c9f6eff23e72d255afbfc74aa024
MohamedSiddiq88 <diddiq88@gmail.com^c>
2023-06-07 16:34:32 +0530

    new

--- Routers/welcome.js
+++ Routers/welcome.js
@@ -5,26 +5,50 @@ import { getMentorById } from "../Controllers/mentors.js";
 const router = express.Router();
 
 
-router.get("/",(req,res)=>{
+router.get("/", (req, res) => {
     const htmlContent = `
-    <h1>Welcome to the Students and Mentors Data Portal</h1>
-    <p>Click <a href="/mentors/all">here</a> to view all Mentors data.</p>
-    <p>Click <a href="/students/all">here</a> to view all student data.</p>
-    <p>end point to add mentor</p>
-    <p>/mentors/add</p>
-    <p>end point to add student</p>
-    <p>/students/add</p>
-    <p>end point to assign a mentor to students by batch</p>
-    <p>/mentors/assign-mentor-by-batch/:mentorId</p>
-    <p>end point to assign a mentor to student</p>
-    <p>/mentors/assign-mentor/:studentId</p>
-    <p>end point to show all students for pertucular mentor</p>
-    <p>/students/filtered-by-mentor/:mentorId</p>
-    <p>end point to show previiously assigned mentor for a perticular student</p>
-    <p>/students/previous-mentor/:studentId</p>
-    <p>Feel free to explore other pages.</p>
-  `;
-  res.send(htmlContent);
-})
+      <html>
+        <head>
+          <style>
+            body {
+              font-family: Arial, sans-serif;
+              padding: 20px;
+            }
+            
+            h1 {
+              color: #333;
+            }
+            
+            p {
+              margin-bottom: 10px;
+            }
+            
+            a {
+              color: #007bff;
+              text-decoration: none;
+            }
+            
+            a:hover {
+              text-decoration: underline;
+            }
+          </style>
+        </head>
+        <body>
+          <h1>Welcome to the Students and Mentors Data Portal</h1>
+          <p>Click <a href="/mentors/all">here</a> to view all Mentors data.</p>
+          <p>Click <a href="/students/all">here</a> to view all student data.</p>
+          <p>Endpoint to add a mentor: <code>/mentors/add</code></p>
+          <p>Endpoint to add a student: <code>/students/add</code></p>
+          <p>Endpoint to assign a mentor to students by batch: <code>/students/assign-mentor-by-batch/:mentorId</code></p>
+          <p>Endpoint to assign a mentor to a student: <code>/students/assign-mentor/:studentId</code></p>
+          <p>Endpoint to show all students for a particular mentor: <code>/students/filtered-by-mentor/:mentorId</code></p>
+          <p>Endpoint to show previously assigned mentor for a particular student: <code>/students/previous-mentor/:studentId</code></p>
+          <p>Feel free to explore other pages.</p>
+        </body>
+      </html>
+    `;
+    res.send(htmlContent);
+  });
+  
 
 export const welcomeRouter = router;

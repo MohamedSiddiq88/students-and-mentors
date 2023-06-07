54acd7e3d52447f87246238477fac0bb3e78324a
MohamedSiddiq88 <diddiq88@gmail.com^c>
2023-06-07 16:27:26 +0530

    new

--- Routers/welcome.js
+++ Routers/welcome.js
@@ -8,7 +8,20 @@ const router = express.Router();
 router.get("/",(req,res)=>{
     const htmlContent = `
     <h1>Welcome to the Students and Mentors Data Portal</h1>
+    <p>Click <a href="/mentors/all">here</a> to view all Mentors data.</p>
     <p>Click <a href="/students/all">here</a> to view all student data.</p>
+    <p>end point to add mentor</p>
+    <p>/mentors/add</p>
+    <p>end point to add student</p>
+    <p>/students/add</p>
+    <p>end point to assign a mentor to students by batch</p>
+    <p>/mentors/assign-mentor-by-batch/:mentorId</p>
+    <p>end point to assign a mentor to student</p>
+    <p>/mentors/assign-mentor/:studentId</p>
+    <p>end point to show all students for pertucular mentor</p>
+    <p>/students/filtered-by-mentor/:mentorId</p>
+    <p>end point to show previiously assigned mentor for a perticular student</p>
+    <p>/students/previous-mentor/:studentId</p>
     <p>Feel free to explore other pages.</p>
   `;
   res.send(htmlContent);
